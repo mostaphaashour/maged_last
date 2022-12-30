776b793cad148e3555c96cc1319a7e53edabbd47
Mostapha Ashour <74195503+mostaphaashour@users.noreply.github.com>
2022-12-30 17:56:14 +0200

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,92 @@
+// let sarfmlabs = document.querySelector("#sarfmlabs");
+// let ertega3mlabs = document.querySelector("#ertega3mlabs");
+// let gardmlabs = document.querySelector("#gardmlabs");
+// let sa7borertega3 = document.querySelector(".sa7borertega3");
+// console.log(sarfmlabs);
+// console.log(ertega3mlabs);
+
+// sarfmlabs.addEventListener("click", () => {
+//   sa7borertega3.innerHTML = "صرف";
+// });
+// ertega3mlabs.addEventListener("click", () => {
+//   sa7borertega3.innerHTML = "ارتجاع";
+//   console.log("er");
+// });
+
+// let dark = document.querySelector(".theme");
+// let check = document.querySelector("#cb");
+// if (localStorage.getItem("dark") === "dark") {
+//   dark.classList.add("dark");
+//   check.checked = true;
+// }
+
+// check.addEventListener("click", () => {
+//   dark.classList.toggle("dark");
+//   localStorage.setItem("dark", dark.classList.item(1));
+// });
+
+// let sa7boreda3 = document.querySelector(".sa7boreda3");
+// let psa7boreda3 = document.querySelector(".psa7boreda3");
+// let save = document.querySelector(".save");
+// let cancel = document.querySelector(".cancel");
+// let cards = document.querySelector(".cards");
+// let head = document.querySelector(".head");
+// let lef = document.querySelector(".lef");
+// let notifications = document.querySelector(".notifications");
+// sa7b.addEventListener("click", () => {
+//   cards.style.cssText = `
+//   opacity: 40%
+//   `;
+//   psa7boreda3.innerHTML = "سحب";
+//   save.innerHTML = "سحب";
+//   head.style.cssText = `
+//   opacity: 40%
+//   `;
+//   sa7boreda3.style.cssText = `
+//   display:  block;
+//   `;
+// });
+// eda3.addEventListener("click", () => {
+//   head.style.cssText = `
+//   opacity: 40%
+//   `;
+//   cards.style.cssText = `
+//   opacity: 40%
+//   `;
+//   save.innerHTML = "ايداع";
+//   psa7boreda3.innerHTML = "ايداع";
+//   sa7boreda3.style.cssText = `
+//   display:  block;
+//   `;
+// });
+
+// save.addEventListener("click", () => {
+//   head.style.cssText = `
+//   opacity: 100%
+//   `;
+//   cards.style.cssText = `
+//   opacity: 100%
+//   `;
+//   sa7boreda3.style.cssText = `
+//   display:  none;
+//   `;
+// });
+
+// cancel.addEventListener("click", () => {
+//   head.style.cssText = `
+//   opacity: 100%
+//   `;
+//   cards.style.cssText = `
+//   opacity: 100%
+//   `;
+//   sa7boreda3.style.cssText = `
+//   display:  none;
+//   `;
+// });
+
+// lef.addEventListener("click", () => {
+//   notifications.style.display = "block";
+//   setTimeout(() => {
+//     notifications.style.display = "none";
+//   }, 5000);
+// });
